a17b61a1d24c07c9eae00f0db5a5a5162285991b
Fidel667 <107774292+Fidel667@users.noreply.github.com>
2026-05-28 15:02:40 -0400

    Delete react-shop/src directory

--- react-shop/src/components/ProductCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProductCard({ product, onClick }) {
-  return (
-    <div className="product-card" onClick={() => onClick(product.id)}>
-      <img src={product.thumbnail} alt={product.title} />
-      <h3>{product.title}</h3>
-      <p className="price">${product.price.toFixed(2)}</p>
-    </div>
-  );
-}
